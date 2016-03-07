250d9ac0465ecef0cd7fdf506b36a91dc6da69f8
data-kid <gabriel.grover@gmail.com>
2016-03-07 09:54:22 -0800

    implemented router helper

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 "use strict"
 const express = require('express');
 const app = express();
+const chatCat = require('./app');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res, next) => {
-  //res.send('<h1>Hello Express</h1>');
-  res.render('login', {
-    pageTitle:'My Login Page'
-  });
-});
+app.use('/', chatCat.router);
 
 app.listen(app.get('port'), () => {
   console.log('ChatCat running on port 3000');
