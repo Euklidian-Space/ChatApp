431a78939663fc59ff930268d3c98fc546c9f927
data-kid <gabriel.grover@gmail.com>
2016-03-06 22:39:41 -0800

    installed ejs

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 "use strict"
 const express = require('express');
 const app = express();
+
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
-  res.send('<h1>Hello Express</h1>');
-});
-
-app.get('/dashboard', (res,req,next) => {
-  res.send('<h1>this is the dashboard page</h1>');
+  //res.send('<h1>Hello Express</h1>');
+  res.render('login', {
+    pageTitle:'My Login Page'
+  });
 });
 
 app.listen(app.get('port'), () => {
